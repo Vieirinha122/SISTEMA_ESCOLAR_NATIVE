8d472f0f4ee7094baaeb0257b96a60769bd3946e
Arthur Vieira <arthurv720@gmail.com>
2024-11-12 20:39:23 -0300

    - Projeto inicializado - Adicionado e testado HomeScreen (falta puxar a turma) - Adicionado e testado LoginScreen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,117 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import axios from 'axios';
+
+export default function HomeScreen({ route, navigation }) {
+  const { aluno } = route.params;
+  const [nomeTurma, setNomeTurma] = useState('');
+
+   // Função para buscar o nome da turma
+   const fetchTurma = async (idTurma) => {
+    try {
+      const response = await axios.get(`https://sistema-escolar-two.vercel.app/api/turmas/${idTurma}`);
+      setNomeTurma(response.data.nome); // Exibe o nome da turma
+    } catch (error) {
+      console.error('Erro ao buscar nome da turma:', error);
+      setError('Erro ao buscar nome da turma.');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.studentCard}>
+        <MaterialIcons name="person" size={40} color="#004B8D" />
+        <Text style={styles.studentText}>ALUNO: {aluno.nome}</Text>
+        <Text style={styles.studentText}>{aluno.matricula} - Ano: 2024</Text>
+        <Text style={styles.studentText}>{nomeTurma} - {aluno.turno}</Text>
+      </View>
+
+      {/* Botões */}
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.button}>
+          <MaterialIcons name="school" size={40} color="#004B8D" />
+          <Text style={styles.buttonText}>Conceitos</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <MaterialIcons name="warning" size={40} color="#004B8D" />
+          <Text style={styles.buttonText}>Avisos</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+        <Image source={require('../assets/Geekie_One.png')} style={styles.logoGeekie} />
+          <Text style={styles.buttonText}>GeekieOne</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <MaterialIcons name="calendar-month" size={40} color="#004B8D" />
+          <Text style={styles.buttonText}>Horários</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
+          <MaterialIcons name="door-back" size={40} color="#004B8D" />
+          <Text style={styles.buttonText}>Sair</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Logo */}
+      <View style={styles.logoContainer}>
+        <Image source={require('../assets/Logo_senac.png')} style={styles.logo} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#014E90',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      paddingVertical: 20,
+    },
+    studentCard: {
+      backgroundColor: 'white',
+      padding: 15,
+      marginTop: 70,
+      borderRadius: 50,
+      alignItems: 'center',
+      width: '70%',
+    },
+    studentText: {
+      fontSize: 16,
+      color: '#1A73BA',
+      fontWeight: 'bold',
+    },
+    buttonsContainer: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+      width: '80%',
+    },
+    button: {
+      backgroundColor: 'white',
+      borderRadius: 10,
+      paddingVertical: 20,
+      paddingHorizontal: 30,
+      margin: 10,
+      alignItems: 'center',
+      width: '40%',
+    },
+    buttonText: {
+      color: '#1A73BA',
+      fontSize: 15,
+      fontWeight: 'bold',
+    },
+    logoContainer: {
+      alignItems: 'center',
+    },
+    logo: {
+      width: 120,
+      height: 70,
+      resizeMode: 'contain',
+      alignItems: 'center'
+    },
+    logoGeekie: {
+        width: 70,
+        height: 40,
+        objectFit: 'contain'
+    }
+  });
---
+++ screens/LoginScreen.js
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, Image } from 'react-native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import axios from 'axios';
+
+export default function LoginScreen({ navigation }) {
+  const [matricula, setMatricula] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('https://sistema-escolar-two.vercel.app/api/login', {
+        matricula: matricula,
+        senha: senha,
+      });
+
+      if (response.data.success) {
+        const aluno = response.data.aluno; // Acesse os dados do aluno da resposta
+        navigation.navigate('Home', { aluno });
+      } else {
+        Alert.alert('Erro', response.data.message || 'Matrícula ou senha incorreta');
+      }
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Erro', 'Houve um erro ao tentar fazer login. Tente novamente mais tarde.');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.loginBox}>
+        <Image source={require('../assets/Senac_Mediotec.png')} style={styles.logo} />
+
+        <Text style={styles.label}>Matrícula</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Matrícula"
+            value={matricula}
+            onChangeText={setMatricula}
+            keyboardType="numeric"
+          />
+          <MaterialIcons name="person" size={20} color="#004B8D" style={styles.icon} />
+        </View>
+
+        <Text style={styles.label}>Senha</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Senha"
+            value={senha}
+            onChangeText={setSenha}
+            secureTextEntry
+          />
+          <MaterialIcons name="lock" size={20} color="#004B8D" style={styles.icon} />
+        </View>
+
+        <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+          <Text style={styles.loginButtonText}>Entrar</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00478F',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loginBox: {
+    width: '80%',
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'center',
+  },
+  logo: {
+    width: 120, 
+    height: 90,
+    marginBottom: 30, 
+    objectFit: 'contain'
+  },
+  label: {
+    alignSelf: 'flex-start',
+    marginBottom: 5,
+    fontSize: 18,
+    color: '#00478F',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#00478F',
+    marginBottom: 20,
+    width: '100%',
+  },
+  input: {
+    flex: 1,
+    paddingVertical: 5,
+  },
+  icon: {
+    marginLeft: 10,
+  },
+  loginButton: {
+    backgroundColor: '#00478F',
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    width: '100%',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  loginButtonText: {
+    color: '#fff',
+    fontSize: 20,
+  },
+});
