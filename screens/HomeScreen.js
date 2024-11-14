3260f67ea4f69b4645c47b627ac6762fd904b181
Arthur Vieira <arthurv720@gmail.com>
2024-11-14 11:10:24 -0300

    - Corrigido erro de não aparecer turma referente ao aluno na tela principal

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import axios from 'axios';
 
@@ -7,16 +7,27 @@ export default function HomeScreen({ route, navigation }) {
   const { aluno } = route.params;
   const [nomeTurma, setNomeTurma] = useState('');
 
-   // Função para buscar o nome da turma
-   const fetchTurma = async (idTurma) => {
+
+  const fetchTurma = async (idTurma) => {
     try {
+      console.log("Fetching turma with ID:", idTurma);
       const response = await axios.get(`https://sistema-escolar-two.vercel.app/api/turmas/${idTurma}`);
-      setNomeTurma(response.data.nome); // Exibe o nome da turma
+      setNomeTurma(response.data.nome); 
+      console.log("Nome da turma:", response.data.nome); 
     } catch (error) {
       console.error('Erro ao buscar nome da turma:', error);
-      setError('Erro ao buscar nome da turma.');
     }
   };
+
+
+  useEffect(() => {
+
+    if (aluno && Array.isArray(aluno.turma) && aluno.turma.length > 0) {
+      fetchTurma(aluno.turma[0]); 
+    } else {
+      console.log("ID da turma não encontrado.");
+    }
+  }, [aluno]);
 
   return (
     <View style={styles.container}>
@@ -24,10 +35,10 @@ export default function HomeScreen({ route, navigation }) {
         <MaterialIcons name="person" size={40} color="#004B8D" />
         <Text style={styles.studentText}>ALUNO: {aluno.nome}</Text>
         <Text style={styles.studentText}>{aluno.matricula} - Ano: 2024</Text>
-        <Text style={styles.studentText}>{nomeTurma} - {aluno.turno}</Text>
+        <Text style={styles.studentText}>{nomeTurma ? `${nomeTurma} - ${aluno.turno}` : 'Carregando turma...'}</Text>
       </View>
 
-      {/* Botões */}
+
       <View style={styles.buttonsContainer}>
         <TouchableOpacity style={styles.button}>
           <MaterialIcons name="school" size={40} color="#004B8D" />
@@ -38,7 +49,7 @@ export default function HomeScreen({ route, navigation }) {
           <Text style={styles.buttonText}>Avisos</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.button}>
-        <Image source={require('../assets/Geekie_One.png')} style={styles.logoGeekie} />
+          <Image source={require('../assets/Geekie_One.png')} style={styles.logoGeekie} />
           <Text style={styles.buttonText}>GeekieOne</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.button}>
@@ -51,7 +62,6 @@ export default function HomeScreen({ route, navigation }) {
         </TouchableOpacity>
       </View>
 
-      {/* Logo */}
       <View style={styles.logoContainer}>
         <Image source={require('../assets/Logo_senac.png')} style={styles.logo} />
       </View>
@@ -60,58 +70,58 @@ export default function HomeScreen({ route, navigation }) {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#014E90',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      paddingVertical: 20,
-    },
-    studentCard: {
-      backgroundColor: 'white',
-      padding: 15,
-      marginTop: 70,
-      borderRadius: 50,
-      alignItems: 'center',
-      width: '70%',
-    },
-    studentText: {
-      fontSize: 16,
-      color: '#1A73BA',
-      fontWeight: 'bold',
-    },
-    buttonsContainer: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'center',
-      width: '80%',
-    },
-    button: {
-      backgroundColor: 'white',
-      borderRadius: 10,
-      paddingVertical: 20,
-      paddingHorizontal: 30,
-      margin: 10,
-      alignItems: 'center',
-      width: '40%',
-    },
-    buttonText: {
-      color: '#1A73BA',
-      fontSize: 15,
-      fontWeight: 'bold',
-    },
-    logoContainer: {
-      alignItems: 'center',
-    },
-    logo: {
-      width: 120,
-      height: 70,
-      resizeMode: 'contain',
-      alignItems: 'center'
-    },
-    logoGeekie: {
-        width: 70,
-        height: 40,
-        objectFit: 'contain'
-    }
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#014E90',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+  },
+  studentCard: {
+    backgroundColor: 'white',
+    padding: 15,
+    marginTop: 70,
+    borderRadius: 50,
+    alignItems: 'center',
+    width: '70%',
+  },
+  studentText: {
+    fontSize: 16,
+    color: '#1A73BA',
+    fontWeight: 'bold',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    width: '80%',
+  },
+  button: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 30,
+    margin: 10,
+    alignItems: 'center',
+    width: '40%',
+  },
+  buttonText: {
+    color: '#1A73BA',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  logoContainer: {
+    alignItems: 'center',
+  },
+  logo: {
+    width: 120,
+    height: 70,
+    resizeMode: 'contain',
+    alignItems: 'center'
+  },
+  logoGeekie: {
+    width: 70,
+    height: 40,
+    resizeMode: 'contain',
+  }
+});
